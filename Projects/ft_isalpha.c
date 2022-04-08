f67668c1d61cc2988fa2e98a4046549596fb2102
Annie Graetz <anniegraetz@Annies-MBP-2.lan>
2022-04-08 10:00:09 +0930

    Revert "Update ft_isalpha.c"
    This reverts commit 585f2b0b4f757f952aba9bc28eba93021c0e8c0e.

--- Projects/ft_isalpha.c
+++ Projects/ft_isalpha.c
@@ -10,8 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
- #include <stdio.h>
-int main()
+#include "libft.h"
+
+int	ft_isalpha(int i)
 {
-	prinft("Hello from Lori!!");
+	if ((i >= 65 && i <= 90) || (i >= 97 && i <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
